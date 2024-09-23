b6c7ef5e28b928a6c3b09a43e82d564a5347cecc
jhosuapp <jhosuapvll@gmail.com>
2024-09-22 20:52:28 -0500

    feat: config tailwind and global css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,46 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
+import { Colors } from './app/config/';
+const {nextui} = require("@nextui-org/react");
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './shared/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      colors: Colors,
+      fontFamily: {
+        plusJakarta: ['var(--font-plusJakarta)']
+      },
+      fontSize: {
+        'title': '32px',
+      },
+      maxWidth: {
+        'container': '100%',
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
-  plugins: [],
-};
-export default config;
+  plugins: [nextui({
+    themes: {
+      light: {
+        colors: {
+          primary: {
+            foreground: Colors.utility.black
+          },
+          secondary: {
+            foreground: Colors.utility.black,
+            DEFAULT: Colors.utility.white,
+          }
+        },
+      },
+    }
+  })],
+}
+export default config
